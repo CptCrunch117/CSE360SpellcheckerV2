884e5f6cc4bb68364fcaa28e77bb39a15a94283d
Andrew Gregory <angregor@asu.edu>
2015-12-08 16:41:44 -0700

    Update SpellChecker.java
    copied in the GUI stuff

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
  * Created by CptAmerica on 12/8/15.
  */
 public class SpellChecker {
+    
+    private JFrame frame;
 
     public static void main(String[] args) throws IOException {
-
+        EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SpellcheckerGUI window = new SpellcheckerGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
         Dictionary dict = new Dictionary();
         /*dict.insertWord("A");
         dict.insertWord("D");
@@ -96,7 +107,41 @@ public class SpellChecker {
         }
         System.exit(0);
     }
+/**
+	 * Create the application.
+	 */
+	public SpellcheckerGUI() {
+		initialize();
+	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Lucida Grande", Font.PLAIN, 33));
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JTextArea txtrWordsWillAppear = new JTextArea();
+		txtrWordsWillAppear.setFont(new Font("Lucida Grande", Font.PLAIN, 33));
+		txtrWordsWillAppear.setRows(1);
+		txtrWordsWillAppear.setText("Words Will Appear Here");
+		frame.getContentPane().add(txtrWordsWillAppear, BorderLayout.NORTH);
+		
+		JButton btnNext = new JButton("Ignore");
+		btnNext.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		frame.getContentPane().add(btnNext, BorderLayout.WEST);
+		
+		JButton btnAddWord = new JButton("Add Word");
+		frame.getContentPane().add(btnAddWord, BorderLayout.EAST);
+		
+		JButton btnNext_1 = new JButton("Next");
+		frame.getContentPane().add(btnNext_1, BorderLayout.CENTER);
+	}
 
 
 }
